63e8f3441f9df99cd1c0e44003dff5594ab798c3
Liminal Directory <148764889+LiminalDirectory@users.noreply.github.com>
2024-11-07 06:01:36 -0500

    Update game.js
    Added gamestate.cards variable and added Scene2 to the scene list

--- game/game.js
+++ game/game.js
@@ -2,6 +2,7 @@
 
 //A global object to pass values between functions and files alike
 const gameState = {
+  cards: [0, 0, 1],
   nextScene: false, //Scene flag for changing to the next scene
   escape: false, //Is set to true in some scene files when the player presses esc
 };
@@ -11,13 +12,13 @@ const config = {
   height: 512,
   width: 512,
   backgroundColor: 0x660035, //My palette: 140026-660035-bf1600-ff7300 (reds), or 240740-3a3aa6-12b3a2-45e67d (blues), or 080033-2d0059-a600a6-f29dd6 (purples)
-  scene: [Title, Scene1],
+  scene: [Title, Scene1, Scene2],
   pixelArt: true, //Turns off anti-aliasing
   physics: {
     default: 'arcade',
     arcade: {
       //gravity: { y: 0 }, not sure if I'll need gravity or not
-      debug: false, //Set to true to turn on debug mode (shows hitboxes and velocities)
+      debug: true, //Set to true to turn on debug mode (shows hitboxes and velocities)
     }
   },
   scale: {
